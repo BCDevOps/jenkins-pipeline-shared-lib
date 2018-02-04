3dfe6e3ff6ecdd489a11f500e8afdd5fcdccc2e0
Angelika Ehlers <angelika.ehlers@gov.bc.ca>
2018-02-04 12:52:28 -0800

    Create ChangedFiles.groovy

---
+++ src/bcgov/ChangedFiles.groovy
@@ -0,0 +1,15 @@
+def changes = "Changes:\n"
+build = currentBuild
+while(build != null && build.result != 'SUCCESS') {
+    changes += "In ${build.id}:\n"
+    for (changeLog in build.changeSets) {
+        for(entry in changeLog.items) {
+            for(file in entry.affectedFiles) {
+                changes += "* ${file.path}\n"
+            }
+        }
+    }
+    build = build.previousBuild
+}
+return changes
+return this
