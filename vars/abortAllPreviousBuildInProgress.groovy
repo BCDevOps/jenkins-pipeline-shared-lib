162ed09ecf395945a8b3b16a76063296f9b7b59a
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-05-14 19:50:20 -0700

    Improved abort step

--- vars/abortAllPreviousBuildInProgress.groovy
+++ vars/abortAllPreviousBuildInProgress.groovy
@@ -1,6 +1,28 @@
 
+@NonCPS
+private void abortBuild(build){
+    boolean aborted=false;
+    if (build instanceof org.jenkinsci.plugins.workflow.job.WorkflowRun){
+        for (org.jenkinsci.plugins.workflow.support.steps.input.InputAction inputAction:build.getActions(org.jenkinsci.plugins.workflow.support.steps.input.InputAction.class)){
+            for (org.jenkinsci.plugins.workflow.support.steps.input.InputStepExecution inputStep:inputAction.getExecutions()){
+                if (!inputStep.isSettled()){
+                    inputStep.doAbort()
+                    while (build.isInProgress()){
+                        Thread.sleep(1000) //milliseconds
+                    }
+                }
+            }
+        }
+    }
+
+    if (build.isInProgress()){
+        build.doKill()
+    }
+
+}
+
 def call(currentBuild) {
     while(currentBuild.rawBuild.getPreviousBuildInProgress() != null) {
-        currentBuild.rawBuild.getPreviousBuildInProgress().doKill()
+        abortBuild(currentBuild.rawBuild.getPreviousBuildInProgress())
     }
 }
