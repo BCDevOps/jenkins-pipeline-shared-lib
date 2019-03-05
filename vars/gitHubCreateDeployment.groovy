6a37b843090cd0003a94a23d6201ce34fe402b4b
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2019-03-04 22:50:28 -0800

    Update gitHubCreateDeployment.groovy

--- vars/gitHubCreateDeployment.groovy
+++ vars/gitHubCreateDeployment.groovy
@@ -3,7 +3,7 @@ import org.kohsuke.github.*
 import com.cloudbees.jenkins.GitHubRepositoryName
 
 
-def call(script, environment) {
+def call(script, environment, options = null) {
   def commitId = sh(returnStdout: true, script: 'git rev-parse HEAD')
   String gitUrl = script.scm.getUserRemoteConfigs()[0].getUrl()
   GHRepository repository = GitHubRepositoryName.create(gitUrl).resolveOne()
