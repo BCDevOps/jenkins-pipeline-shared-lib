28578e871cb909e8cc893218a69cd9f7291f1ff6
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2019-03-04 22:35:32 -0800

    Update gitHubCreateDeployment.groovy

--- vars/gitHubCreateDeployment.groovy
+++ vars/gitHubCreateDeployment.groovy
@@ -1,8 +1,14 @@
 import bcgov.GitHubHelper
+import org.kohsuke.github.*
 
-def call(script, payload) {
+def call(script, environment) {
   def commitId = sh(returnStdout: true, script: 'git rev-parse HEAD')
   String gitUrl = script.scm.getUserRemoteConfigs()[0].getUrl()
+  GHRepository repository = GitHubRepositoryName.create(gitUrl).resolveOne()
+  GHDeploymentBuilder builder = repository.createDeployment(commitId)
+  builder.environment(environment)
+  builder.autoMerge(false)
+  builder.requiredContexts([])
   
-  return GitHubHelper.createDeployment(gitUrl, commitId, payload)
+  return builder.create().getId()
 }
