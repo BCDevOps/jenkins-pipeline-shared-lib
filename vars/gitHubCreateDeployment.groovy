da3b042ac887b9723e12d1ec52e08cfc8c0dc440
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2019-03-04 22:36:40 -0800

    Update gitHubCreateDeployment.groovy

--- vars/gitHubCreateDeployment.groovy
+++ vars/gitHubCreateDeployment.groovy
@@ -1,5 +1,7 @@
 import bcgov.GitHubHelper
 import org.kohsuke.github.*
+import com.cloudbees.jenkins.GitHubRepositoryName
+
 
 def call(script, environment) {
   def commitId = sh(returnStdout: true, script: 'git rev-parse HEAD')
